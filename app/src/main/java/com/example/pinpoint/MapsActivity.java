a4ae87a08cd2bc2f21d69594ce5a0289c0bb1a5f
Berk Akgül <berkak_gul51@hotmail.com>
2024-05-28 11:52:34 +0300

    Permissions

--- app/src/main/java/com/example/pinpoint/MapsActivity.java
+++ app/src/main/java/com/example/pinpoint/MapsActivity.java
@@ -1,13 +1,23 @@
 package com.example.pinpoint;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,11 +26,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.pinpoint.databinding.ActivityMapsBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    ActivityResultLauncher<String> permissionLauncher;
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        registerLauncher();
     }
 
 
@@ -40,8 +56,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LocationManager locationManager =(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locationListener = new LocationListener() {
+         locationManager =(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
                 System.out.println("location: "+location.toString());
@@ -49,9 +65,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         };
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+           if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_COARSE_LOCATION)){
+               Snackbar.make(binding.getRoot(),"Permission needed for maps",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                    permissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+                   }
+               }).show();
+           } else {
+               permissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
+           }
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+        }
+
+
+
         // Add a marker in Sydney and move the camera
         LatLng latLng = new LatLng(42.9628054,17.1217439);
         mMap.addMarker(new MarkerOptions().position(latLng).title("Berk"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,5));
     }
+
+    private void registerLauncher(){
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean o) {
+                if(o){
+                    if(ContextCompat.checkSelfPermission(MapsActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                    }
+
+                }else{
+                    Toast.makeText(MapsActivity.this,"Permission Needed!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 }
