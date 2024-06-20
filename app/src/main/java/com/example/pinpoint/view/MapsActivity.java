0e97839cdf9d293bdf7bb5165bff4debe2040059
Berk Akgül <berkak_gul51@hotmail.com>
2024-06-20 15:10:53 +0300

    RxJava

--- app/src/main/java/com/example/pinpoint/view/MapsActivity.java
+++ app/src/main/java/com/example/pinpoint/view/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.pinpoint.view;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -11,6 +12,7 @@ import androidx.room.Room;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -33,6 +35,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.pinpoint.databinding.ActivityMapsBinding;
 import com.google.android.material.snackbar.Snackbar;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
@@ -46,6 +53,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     PlaceDao placeDao;
     Double selectedLatitude;
     Double selectedLongitude;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -167,11 +175,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void save(View view){
 
         Place place=new Place(binding.placeNameText.getText().toString(),selectedLatitude,selectedLongitude);
-        placeDao.insert(place);
+        placeDao.insert(place).subscribeOn(Schedulers.io()).subscribe();
+
+        compositeDisposable.add(placeDao.insert(place)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(MapsActivity.this::handleResponse)
+        );
 
     }
 
-    public void delete(View view){
+    private void handleResponse(){
+        Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 
+    public void delete(View view){
+       /* compositeDisposable.add(placeDao.delete()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(MapsActivity.this::handleResponse)
+
+        );
+
+        */
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 }
